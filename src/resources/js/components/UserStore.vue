e19b06c62772f280af718b3c9674650b5e28beb7
Sam H <s.hopkinson87@gmail.com>
2020-04-25 12:30:49 +0100

    continued additions in styles

--- src/resources/js/components/UserStore.vue
+++ src/resources/js/components/UserStore.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex flex-col justify-around align-center container mx-auto w-1/2 h-screen">
         <h4 class="font-bold font-display text-1xl text-center">Create your user</h4>
-        <form class="flex flex-col container mx-auto w-full"
+        <form class="flex flex-row flex-wrap container mx-auto w-full"
               @submit.prevent="submit">
             <div>
                 <input type="text" class="rounded-lg
-                    p-2 w-1/2 border-4 border-gray-400 border-solid
+                    p-2 w-5/6 border-4 border-gray-400 border-solid
                      my-2 flex justify-center align-center
                       content-center" name="name" id="name" placholder="name" v-model="fields.name"/>
                 <div v-if="errors && errors.name" class="text-danger">{{ errors.name[0] }}</div>
@@ -13,7 +13,7 @@
 
             <div>
                 <input type="email" class="rounded-lg
-                    p-2 w-1/2 border-4 border-gray-400 border-solid
+                    p-2 w-5/6 border-4 border-gray-400 border-solid
                      my-2 flex justify-center align-center
                       content-center" name="email" id="email" v-model="fields.email"/>
                 <div v-if="errors && errors.email" class="text-danger">{{ errors.email[0] }}</div>
@@ -21,13 +21,17 @@
 
             <div>
                 <textarea class="rounded-lg
-                    p-2 w-1/2 border-4 border-gray-400 border-solid
+                    p-2 w-5/6 border-4 border-gray-400 border-solid
                      my-2 flex justify-center align-center
                       content-center" id="message" name="message" rows="5" v-model="fields.message"></textarea>
                 <div v-if="errors && errors.message" class="text-danger">{{ errors.message[0] }}</div>
             </div>
+            <div class="w-4/5 container mx-auto rounded-lg
+                    p-2 w-5/6 border-4 border-gray-400 border-solid
+                     my-2">x</div>
 
-            <button type="submit" class="btn btn-primary">Send message</button>
+
+            <button type="submit" class="w-full btn btn-primary">Send message</button>
         </form>
         </div>
 
